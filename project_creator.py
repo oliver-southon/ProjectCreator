4bdd07a4b6bbaf0ffb2a38ec4b7e73af1da09abf
Oliver <oliversouthon@gmail.com>
2019-11-29 11:23:50 +0800

    first-commit

---
+++ project_creator.py
@@ -0,0 +1,4 @@
+#1. user enters name of project
+#2. determine type (python or web)
+#3. If python makes a dir with project name in Projects folder, plus start.py
+#3. If web, makes dir w/ projectname and files: scipt.js, index.html, styles.css
