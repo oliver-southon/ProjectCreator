65740065dac61e7f5304e268f02da96ba39d1e92
Oliver <oliversouthon@gmail.com>
2019-12-14 21:32:42 +0800

    fifth-commit-gui-input

--- project_creator.py
+++ project_creator.py
@@ -8,34 +8,37 @@ layout = [
     [sg.Text('File Types')],
     [sg.Text('1. Python file (start.py)')],
     [sg.Text('2. Web app (script.js, index.html, styles.css)')],
+    [sg.Text('Choose your file type (1 or 2):'), sg.InputText()],
     [sg.Text('Project Name:'), sg.InputText()],
-    [sg.Text('File type: '), sg.InputText()],
     [sg.Button('Ok'), sg.Button('Cancel')],
 ]
-window = sg.Window('Peep', layout) #make the window
+window = sg.Window('Project Creator', layout) #make the window
 
 event, values = window.read()
-FileName = values[0]
+ProjectName = values[1]
 
-def make_file(x):
-    if x == '1':
-        os.makedirs('../' + FileName)
-        open(f"..\{FileName}\start.py", 'x')
-    elif x == '2':
-        os.makedirs('../' + FileName)
-        open(f"..\{FileName}\index.html", 'x')
-        open(f"..\{FileName}\style.css", 'x')
-        open(f"..\{FileName}\script.js", 'x')
+def make_file_python(ProjectName): #function to make python project
+    os.makedirs('../' + ProjectName)
+    open(f"..\{ProjectName}\start.py", 'x')
+def make_file_webapp(ProjectName): #function to make webapp project
+    os.makedirs('../' + ProjectName)
+    open(f"..\{ProjectName}\index.html", 'x')
+    open(f"..\{ProjectName}\style.css", 'x')
+    open(f"..\{ProjectName}\script.js", 'x')
 
 count = 0
-while True:
+while count < 1:
     if event in (None, 'Cancel'):
         break
-    make_file(values[1])
-    count +=1
-    if count >= 1:
+    elif values[0] == '1':
+        make_file_python(ProjectName)
+        count +=1
+    elif values[0] == '2':
+        make_file_webapp(ProjectName)
+        count +=1
+    elif count >= 1:
         break
 
 window.close()
-#ProjectName = input("Enter the project name: ")
+
 
