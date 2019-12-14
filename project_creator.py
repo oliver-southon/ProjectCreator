92a1d258ac1dfe68447d8e2a92866f73ff25c762
Oliver <oliversouthon@gmail.com>
2019-12-14 22:09:14 +0800

    sixth-commit-gui-checkbox

--- project_creator.py
+++ project_creator.py
@@ -4,18 +4,20 @@ import PySimpleGUIQt as sg
 sg.change_look_and_feel('DarkAmber') #colour
 
 #layout of window
-layout = [ 
-    [sg.Text('File Types')],
-    [sg.Text('1. Python file (start.py)')],
-    [sg.Text('2. Web app (script.js, index.html, styles.css)')],
-    [sg.Text('Choose your file type (1 or 2):'), sg.InputText()],
-    [sg.Text('Project Name:'), sg.InputText()],
-    [sg.Button('Ok'), sg.Button('Cancel')],
+layout = [
+[sg.Frame(layout=[
+    [sg.Checkbox('1. Python file (start.py)', default=False,key='pyfile'),
+     sg.Checkbox('2. Web app (script.js, index.html, styles.css)', default=False,key='webapp')]],
+title='Select File Type from the Checkbox',title_color='red', 
+relief=sg.RELIEF_SUNKEN, tooltip='Use these to set flags')],
+[sg.Text('Project Name:'), sg.InputText()],
+[sg.Submit(), sg.Button('Cancel')],
 ]
+
 window = sg.Window('Project Creator', layout) #make the window
 
 event, values = window.read()
-ProjectName = values[1]
+ProjectName = values[0]
 
 def make_file_python(ProjectName): #function to make python project
     os.makedirs('../' + ProjectName)
@@ -30,10 +32,10 @@ count = 0
 while count < 1:
     if event in (None, 'Cancel'):
         break
-    elif values[0] == '1':
+    elif values['pyfile'] == True:
         make_file_python(ProjectName)
         count +=1
-    elif values[0] == '2':
+    elif values['webapp'] == True:
         make_file_webapp(ProjectName)
         count +=1
     elif count >= 1:
